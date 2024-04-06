b3257ebf27288a65ab6f4cee054d9dfc0fa95067
Marebella <156524531+Marebella@users.noreply.github.com>
2024-04-06 16:59:44 +0300

    palju muudatusi

--- front-page.php
+++ front-page.php
@@ -1,10 +1,14 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$hero_title=get_field(`hero_title`)
+?>
 
 <main>
 
     <section class="top-section">
         <div class="content-container">
             <h1 class="size-h-3 font-oswald-light top-section-heading">
+                <?php echo esc_html($hero_title)?>
                 Sinu sõna on mu jalale lambiks ja valguseks mu teerajal
             </h1>
             <p>
@@ -30,7 +34,7 @@
     </section>
 
     <section class="mid-section">
-        <div class="content-container">
+        <div class="content-container-md">
             <h2 class="size-h-1 font-oswald mid-section-heading">
             Sest maa saab täis Issanda au tundmist - <br> otsekui veed katavad merepõhja.
             </h2>
@@ -54,6 +58,8 @@
                 <p class= "size-sm barlow-regular text-and-image-section-text">
                    Jumala Sõna on vaim ja elu, sellel on eluandev, vabastav, tervendav, uueks loov ja üles ehitav ilmutuslik vägi. Ja kuna Jumala Sõna kõneleb usu kaudu südamesse, siis pole vahet, kui noor või vana on selle lugeja. 
                    <br>
+                   <br>
+                   <br>
                    Paar lihtsat nõuannet Piibli lugemiseks on:
                    <ul>
                        <li>Leia lugemiseks rahulik aeg ja koht, kus pole segajaid.</li>
@@ -61,9 +67,9 @@
                        <li>Loe valitud piibliteksti aeglaselt, tähelepanelikult ja mitu korda, kuni suudad nimetada loetu peamised teemad. </li>
                        <li>Soovitav on lugeda kõva häälega ette vähemalt ühel korral.</li>
                    </ul>
-                   
-                   
-                   
+                  <a href="" class="btn-main">
+                    LOE LISA SIIT                 
+                  </a>
                
                </p>
            </div>
@@ -72,7 +78,67 @@
            </div>
 
        </div>
+       <p class="vertical-text">AVA PIIBEL</p>
     </section>
-</main>
 
+<section class="text-and-image-section-2">
+       <div class="content-container-pl text-and-image-section-2__container">
+           <div class="text-and-image-section-2__text">
+                 <h2 class= "size-h-4 barlow-medium text-and-image-section-heading">
+                Kuidas palvetada?</h2>
+                <a href="" class="btn-main btn-main--light mt-500 z-index-10">
+                    LOE LISA SIIT                 
+                </a>
+           </div>
+           <div class="text-and-image-section-2__img-1">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/text-and-image-section-2-img 1.png" alt="" class="">
+           </div>
+           </div>
+           <div class="text-and-image-section-2__img-2">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/text-and-image-section-2-img 2.png" alt="" class="">
+           </div>
+
+           </div>
+       <p class="vertical-text vertical-text-dark"> – JUMAL</p>
+</section>
+
+<section class="text-and-image-section-3">
+    <div class="content-container-pl text-and-image-section-3__container">
+        <div class="text-and-image-section-3__text">
+            <div class="text-and-image-section-3__text__inner">
+                <h2 class= "size-h-4 barlow-medium text-and-image-section-heading">
+                    Igapäevane lugemine
+                </h2>
+                <p class= "size-lg barlow-regular text-and-image-section-content">
+                Jumala Sõna on kristlase jaoks usuelu alus. 
+                Kui tahad kasvada vaimulikult, siis on oluline end hoida igapäevaselt Jumala Sõnast toidetuna.</p>
+                <p class= "size-sm barlow-regular text-and-image-section-text">
+                Piiblit võib proovida lugeda algusest lõpuni, mitmest kohast korraga, juhuslikult avanevaid tekste, 
+                kirjakohti märksõnade järgi või muul viisil. Pakume Sulle selleks mitmeid võimalusi.</p>
+             
+                <div class="text-bible-verse">
+                    <div class="text-and-image-section-3__img-1">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/text-and-image-section-3__img-3.png" alt="" class="">
+                    </div>
+                    <p class= "size-italic barlow-italic text-and-image-section-text">
+                        Jeesus, Jh 6:48
+                    </p>
+                </div>
+                <a href="" class="btn-main">
+                    LOE LISA SIIT                 
+                </a>  
+            </div>
+        </div>
+        <div class="text-and-image-section__image-container">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/text-and-image-section-3__img-1.png" alt="" class="text-and-image-section__image-container__img-1">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/text-and-image-section-3__img-2.png" alt="" class="text-and-image-section__image-container__img-2">
+        </div>
+       
+    </div>
+    
+    <p class="vertical-text vertical-text-light">   KÕNELEB!</p>
+
+
+</section>
+</main>
 <?php get_footer(); ?>
